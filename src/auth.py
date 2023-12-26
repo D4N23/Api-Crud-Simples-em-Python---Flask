473633af8e12c11b0601eeef9ff02985bf50d91f
Danilo <daniilosam23@gmail.com>
2023-12-25 22:24:36 -0300

    include respose

--- src/auth.py
+++ src/auth.py
@@ -1,8 +1,10 @@
+from os import access
 from flask import Blueprint, jsonify, request
 from werkzeug.security import check_password_hash, generate_password_hash
-from src.constants.http_status_code import HTTP_200_OK, HTTP_400_BAD_REQUEST, HTTP_409_CONFLICT
+from src.constants.http_status_code import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_401_UNAUTHORIZED, HTTP_409_CONFLICT
 import validators
 from src.database import User, db
+from flask_jwt_extended import create_access_token, create_refresh_token
 
 auth = Blueprint("auth",__name__,url_prefix="/api/v1/auth")
 
@@ -19,16 +21,16 @@ def register():
     if len(username) < 3:
         return jsonify({'error': "User is too short"}),HTTP_400_BAD_REQUEST
     
-    if not username.isalnum() or "" in username:
+    if not username.isalnum() or " " in username:
         return jsonify({'error': "Username should be alphanumeric, also no spaces"}),HTTP_400_BAD_REQUEST   
     
     if not validators.email(email):
         return jsonify({'error': "Email is not valid"}),HTTP_400_BAD_REQUEST
     
-    if User.queryfilter_by(email=email).first() is not None:
+    if User.query.filter_by(email=email).first() is not None:
         return jsonify({'error': "Email is taken"}),HTTP_409_CONFLICT
     
-    if User.queryfilter_by(username=username).first() is not None:
+    if User.query.filter_by(username=username).first() is not None:
         return jsonify({'error': "UserName is taken"}),HTTP_409_CONFLICT
     
     pwd_hash = generate_password_hash(password)
@@ -38,13 +40,37 @@ def register():
     db.session.commit()
     
     return jsonify({
+        'message': "User Created",
         'user':{
-            'refresh': refresh,
-            'access': access,
-            'username': user.username,
-            'email': user.email
+            'username': username,'email': email
         }
-    }),HTTP_200_OK
+    }),HTTP_201_CREATED
+    
+@auth.post('/login')
+def login():
+    email = request.json.get('email','')
+    password = request.json.get('password','')
+    
+    user = User.query.filter_by(email=email).first()
+    
+    if user:
+        is_pass_correct= check_password_hash(user.password, password)
+        
+        if is_pass_correct:
+            refresh=create_refresh_token(identity=user.id)
+            access=create_access_token(identity=user.id)
+            
+            return jsonify({
+                'user':{
+                    'refresh': refresh,
+                    'access': access,
+                    'username': user.username,
+                    'email': user.email
+                }
+            }), HTTP_200_OK
+    
+    return jsonify({'error': 'Wrong credentials'}), HTTP_401_UNAUTHORIZED
+
 
 @auth.get("/me")
 def me():
