a4918a02b4f22ba3143b65d5de2ec272d9cadc51
Danilo <daniilosam23@gmail.com>
2023-12-26 17:09:28 -0300

    fEAT: METHODS ERROS HANDLERS CREATED

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,8 @@
-from flask import Flask, redirect, request
+from flask import Flask, jsonify, redirect, request
 import os
 from src.auth import auth
 from src.bookmarks import bookmarks
+from src.constants.http_status_code import HTTP_404_NOT_FOUND, HTTP_500_INTERNAL_SERVER_ERROR
 from src.database import db, Bookmark
 from flask_jwt_extended import JWTManager
 
@@ -26,18 +27,27 @@ def create_app(test_config=None):
     
     #inclui app como parametro do JWTManage
     JWTManager(app)
+    app.register_blueprint(auth)
+    app.register_blueprint(bookmarks) 
+    
     
     @app.get('/<short_url>')
-    def redirect_tourl(short_url):
+    def redirect_to_url(short_url):
         bookmark = Bookmark.query.filter_by(short_url=short_url).first_or_404()
         
         if bookmark:
             bookmark.visits = bookmark.visits+1
             db.session.commit()
             return redirect(bookmark.url)
-        
     
-    app.register_blueprint(auth)
-    app.register_blueprint(bookmarks) 
+    
+    @app.errorhandler(HTTP_404_NOT_FOUND) 
+    def handle_404(e):
+        return jsonify({'error': 'Not Found'}), HTTP_404_NOT_FOUND   
+    
+    
+    @app.error_handler(HTTP_500_INTERNAL_SERVER_ERROR)
+    def handle_500(e):
+        return jsonify({'error': 'Something went wrong, we are working on it'}), HTTP_500_INTERNAL_SERVER_ERROR
         
     return app    
